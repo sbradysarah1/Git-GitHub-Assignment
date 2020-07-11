62d12b77ba3882766e003bff53905dccacb3dde1
sbradysarah1 <68165513+sbradysarah1@users.noreply.github.com>
2020-07-11 14:13:15 -0600

    Add files via upload

---
+++ PurchaseReport.java
@@ -0,0 +1,19 @@
+// Sarah Brady
+public class PurchaseReport {
+
+	public static void main(String[] args) {
+		System.out.println("=========================");
+		System.out.println("Purchase Report");
+		System.out.println("=========================");
+		System.out.printf("Shirt             $%5.2f\n",21.99);
+		System.out.printf("Pants             $%5.2f\n",49.97);
+		System.out.printf("Shoes             $%5.2f\n",89.50);
+		System.out.printf("Belt              $%5.2f\n",19.99);
+		System.out.printf("Coat             $%6.2f\n",129.95);
+		System.out.println("=========================");
+		System.out.printf("Total            $%6.2f\n",311.40);
+		
+
+	}
+
+}
